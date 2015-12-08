809db140ed1774794fb0ccad182020a9ce5d4231
Julia <harrisonj2@chc.edu>
2015-12-08 14:36:04 -0500

    meeting adapter done

---
+++ app/src/main/java/com/example/harrisonj2/invite/host_adapeter.java
@@ -0,0 +1,7 @@
+package com.example.harrisonj2.invite;
+
+/**
+ * Created by harrisonj2 on 12/8/2015.
+ */
+public class host_adapeter {
+}
---
+++ app/src/main/java/com/example/harrisonj2/invite/meeting_adapter.java
@@ -0,0 +1,44 @@
+package com.example.harrisonj2.invite;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by harrisonj2 on 12/8/2015.
+ */
+public class meeting_adapter extends ArrayAdapter<Meeting> {
+    Meeting meeting;
+    TextView nameTV, hostTV, locationTV, dateTV, timeTV, descriptionTV;
+
+    public meeting_adapter(Context context, Meeting[] objects){
+        super(context, R.layout.meeting_row, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+
+        View meetingView = inflater.inflate(R.layout.meeting_row, parent, false);
+
+        meeting = getItem(position);
+        nameTV = (TextView) meetingView.findViewById((R.id.nameTV));
+        hostTV = (TextView) meetingView.findViewById((R.id.hostTV));
+        locationTV = (TextView) meetingView.findViewById((R.id.locationTV));
+        dateTV = (TextView) meetingView.findViewById((R.id.dateTV));
+        timeTV = (TextView) meetingView.findViewById((R.id.timeTV));
+        descriptionTV = (TextView) meetingView.findViewById((R.id.descriptionTV));
+
+        nameTV.setText(meeting.getName());
+        hostTV.setText(meeting.getHostId());
+        locationTV.setText(meeting.getLocation());
+        dateTV.setText(meeting.getDate());
+        timeTV.setText(meeting.getTime());
+        descriptionTV.setText(meeting.getDescription());
+
+        return meetingView;
+    }
+}
